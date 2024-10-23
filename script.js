3f897ff7e48fd99fdcceb9615b6f03c5cdb62b20
Alexandre Melo <166271928+AlexandreBMelo@users.noreply.github.com>
2024-10-23 23:55:05 +0000

    Commit 2

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 function gerarTXT() {
+    const data = document.getElementById('data').value;
+    const dirigidaPor = document.getElementById('dirigida-por').value;
+    const anuncios = document.getElementById('anuncios').value;
     const primeiroHino = document.getElementById('primeiro-hino').value;
     const primeiraOracao = document.getElementById('primeira-oracao').value;
-    const anuncios = document.getElementById('anuncios').value;
-    const discursantes = document.getElementById('discursantes').value;
+    const discursante1 = document.getElementById('discursante1').value;
+    const discursante2 = document.getElementById('discursante2').value;
+    const segundoHino = document.getElementById('segundo-hino').value;
+    const discursante3 = document.getElementById('discursante3').value;
     const ultimoHino = document.getElementById('ultimo-hino').value;
     const ultimaOracao = document.getElementById('ultima-oracao').value;
 
     const conteudo = 
+        "Data: " + data + "\n" +
+        "Dirigida por: " + dirigidaPor + "\n" +
+        "Anúncios: " + anuncios + "\n" +
         "1º Hino: " + primeiroHino + "\n" +
         "1º Oração: " + primeiraOracao + "\n" +
-        "Anúncios: " + anuncios + "\n" +
-        "Discursantes: " + discursantes + "\n" +
+        "Discursante 1: " + discursante1 + "\n" +
+        "Discursante 2: " + discursante2 + "\n" +
+        "2º Hino: " + segundoHino + "\n" +
+        "Discursante 3: " + discursante3 + "\n" +
         "Último Hino: " + ultimoHino + "\n" +
         "Última Oração: " + ultimaOracao + "\n";
 
@@ -20,4 +30,3 @@ function gerarTXT() {
     link.download = "reuniao.txt";
     link.click();
 }
-
